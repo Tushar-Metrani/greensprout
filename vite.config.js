6dd63514ba8029480213317590121cadd4f423fc
Tushar-Metrani <165750939+Tushar-Metrani@users.noreply.github.com>
2025-09-26 21:21:19 +0530

    deploy changes

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+  base:"/greensprout",
   server: {
     host: true, // or '0.0.0.0'
   },
